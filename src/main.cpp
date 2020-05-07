40af1e31ee1069e46bde2d616ee19e64f376798b
mikle9997 <mikle9997@mail.ru>
2020-05-07 12:00:30 +0300

    add cxx17 to cmake

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,14 @@
 #include <iostream>
+#include <array>
 #include <omp.h>
 
 
 int main(int, char**)
 {
-    #pragma omp parallel
-    std::cout << "Hello, world!\n";
+	// #pragma omp parallel
+	// std::cout << "Hello, world!" << std::endl;
+
+	std::array<int, 3> a = {1,4,3};
+
+	for (auto i : a) std::cout << i << std::endl;
 }
