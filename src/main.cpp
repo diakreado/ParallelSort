17de282cc7939283d0deeff8dd64a523d0d80c86
mikle9997 <mikle9997@mail.ru>
2020-05-10 02:39:06 +0300

    quick sort - one flow

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,69 @@
 #include <iostream>
-#include <array>
+#include <vector>
 #include <omp.h>
 
+#include <ctime>
+#include <cstdlib>
+
+
+int partition (std::vector<int>& arr, int low, int high)
+{
+	int pivot = arr[high];
+	int i = (low - 1);
+
+	auto swap = [](int* a, int* b)
+	{
+		int t = *a;
+		*a = *b;
+		*b = t;
+	};
+
+	for (int j = low; j <= high- 1; j++)
+	{
+		if (arr[j] <= pivot) 
+		{
+			i++;
+			swap(&arr[i], &arr[j]); 
+		}
+	}
+	swap(&arr[i + 1], &arr[high]); 
+	return (i + 1); 
+}
+
+void quick_sort(std::vector<int>& arr, int low, int high) 
+{
+	if (low < high) 
+	{
+		int pi = partition(arr, low, high); 
+
+		quick_sort(arr, low, pi - 1); 
+		quick_sort(arr, pi + 1, high); 
+	}
+}
 
 int main(int, char**)
 {
 	// #pragma omp parallel
 	// std::cout << "Hello, world!" << std::endl;
 
-	std::array<int, 3> a = {1,4,3};
+	constexpr size_t rand_size = 40;
 
-	for (auto i : a) std::cout << i << std::endl;
+	std::vector<int> a;
+
+	srand(time(0));
+
+	for (size_t i = 0; i < rand_size; ++i)
+	{
+		a.push_back(rand()% 100 + 1);
+	}
+
+	for (auto i : a) { std::cout << i << " "; }
+	std::cout << std::endl;
+
+	quick_sort(a, 0, a.size()-1);
+
+	for (auto i : a) { std::cout << i << " "; }
+	std::cout << std::endl;
 }
+
+
