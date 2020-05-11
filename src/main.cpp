7b5807eb35617f2b37ed1890c243d4f49bcce518
mikle9997 <mikle9997@mail.ru>
2020-05-11 20:52:16 +0300

    code improvements

--- src/main.cpp
+++ src/main.cpp
@@ -10,6 +10,21 @@
 #include <forward_list>
 
 
+enum class AlgoritmType_e {
+	standard_library,
+	sequential,
+	parallel
+};
+
+
+constexpr int             g_cutoff      = 1000;
+const     int             g_srand_init  = 4534534;
+constexpr int             g_numThreads  = 4;
+constexpr size_t          g_rand_size   = 10*1000*1000;
+constexpr AlgoritmType_e  g_algoritm    = AlgoritmType_e::parallel;
+constexpr int             g_gen_limit   = 100;
+
+
 
 template <class ForwardIt>
 void __quicksort(ForwardIt first, ForwardIt last)
@@ -32,12 +47,12 @@ void quicksort(std::vector<int>& arr)
 template <class ForwardIt>
 void __quicksort_p(ForwardIt first, ForwardIt last, int cutoff)
 {
-	if(first == last) return;
-	auto pivot = *std::next(first, std::distance(first,last)/2);
+	if(first == last) { return; }
+	auto pivot = *std::next(first, std::distance(first,last) / 2);
 	ForwardIt middle1 = std::partition(first, last,
-							[pivot](const auto& em){ return em < pivot; });
+	                        [pivot](const auto& em){ return em < pivot; });
 	ForwardIt middle2 = std::partition(middle1, last,
-						[pivot](const auto& em){ return !(pivot < em); });
+	                        [pivot](const auto& em){ return !(pivot < em); });
 
 	if (last - first < cutoff)
 	{
@@ -54,17 +69,13 @@ void __quicksort_p(ForwardIt first, ForwardIt last, int cutoff)
 	}
 }
 
-void quick_sort_p(std::vector<int>& arr)
+void quicksort_p(std::vector<int>& arr)
 {
-
-	constexpr int cutoff = 1000;
-	constexpr int numThreads = 2;
-
-	#pragma omp parallel num_threads(numThreads)
+	#pragma omp parallel num_threads(g_numThreads)
 	{
-		#pragma omp single nowait
+		#pragma omp master
 		{
-			__quicksort_p(arr.begin(), arr.end(), cutoff);
+			__quicksort_p(arr.begin(), arr.end(), g_cutoff);
 		}
 	}
 }
@@ -72,38 +83,34 @@ void quick_sort_p(std::vector<int>& arr)
 
 int main(int, char**)
 {
-	// #pragma omp parallel
-	// std::cout << "Hello, world!" << std::endl;
+	std::vector<int> arr;
 
-	constexpr size_t rand_size = 1000*1000*100;
+	srand(g_srand_init);
 
-	std::vector<int> a;
-	// std::vector<int> b;
-
-	srand(time(0));
-
-	for (size_t i = 0; i < rand_size; ++i)
+	for (size_t i = 0; i < g_rand_size; ++i)
 	{
-		int x = rand()% 100 + 1;
-		a.push_back(x);
+		if constexpr (g_gen_limit == 0) { arr.push_back(rand()); }
+		else                           { arr.push_back(rand() % g_gen_limit); }
 	}
 
-	// get start time
 	double t1 = omp_get_wtime();
 
-	// for (auto i : a) { std::cout << i << " "; }
-	// std::cout << std::endl;
-
-
-	// quicksort(a);
-	quick_sort_p(a);
+	if constexpr (g_algoritm == AlgoritmType_e::sequential)
+	{
+		quicksort(arr);
+	}
+	else if constexpr (g_algoritm == AlgoritmType_e::parallel)
+	{
+		quicksort_p(arr);
+	}
+	else if constexpr (g_algoritm == AlgoritmType_e::standard_library)
+	{
+		std::sort(arr.begin(), arr.end());
+	}
 
 
 	double t2 = omp_get_wtime();
-	printf("%zu,%lf,%d\n", rand_size, t2-t1, omp_get_max_threads());
+	printf("%zu, %lf, %d\n", g_rand_size, t2-t1, g_numThreads);
 
-	// for (auto i : a) { std::cout << i << " "; }
-	// std::cout << std::endl;
+	return 0;
 }
-
-
